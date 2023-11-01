e2e293acd887dd29fe4f6492d9e2cc59c9df6667
Nick Kvizhinadze <nickkvizhinadze@hotmail.com>
2023-11-01 23:17:48 +0400

    Added projects

---
+++ top-down-section/main.cpp
@@ -0,0 +1,119 @@
+#include "raylib.h"
+#include "raymath.h"
+#include "Character.h"
+#include "Prop.h"
+#include "Enemy.h"
+#include <string>
+
+int main()
+{
+    // window dimension
+    int windowDimensions[2]{640, 480};
+
+    // initialize the window
+    InitWindow(windowDimensions[0], windowDimensions[1], "Nick's Top Down");
+
+    // map texture
+    Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+    Vector2 mapPos = {0.0, 0.0};
+    const float mapScale{4.f};
+
+    Character knight = Character{windowDimensions[0], windowDimensions[1]};
+
+    Prop props[2]{
+        Prop{{600.f, 300.f}, LoadTexture("nature_tileset/Rock.png")},
+        Prop{{400.f, 500.f}, LoadTexture("nature_tileset/Log.png")}};
+
+    Enemy goblin{
+        {800.f, 300.f},
+        LoadTexture("characters/goblin_idle_spritesheet.png"),
+        LoadTexture("characters/goblin_run_spritesheet.png"),
+    };
+
+    Enemy slime{
+        {700.f, 500.f},
+        LoadTexture("characters/slime_idle_spritesheet.png"),
+        LoadTexture("characters/slime_run_spritesheet.png"),
+    };
+
+    Enemy *enemies[2]{&goblin, &slime};
+    for (Enemy *enemy : enemies)
+    {
+        enemy->setTarget(&knight);
+    }
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+
+        // start drawing
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
+        // draw the map
+        DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
+
+        // draw props
+        for (auto prop : props)
+        {
+            prop.Reneder(knight.getWorldPos());
+        }
+
+        if (!knight.getAlive())
+        {
+            DrawText("Game Over!", 55.f, 45.f, 40, RED);
+            EndDrawing();
+            continue;
+        }
+        else
+        {
+            std::string knightsHealth = "Health: ";
+            knightsHealth.append(std::to_string(knight.getHealth()), 0, 5);
+            DrawText(knightsHealth.c_str(), 55.f, 45.f, 40, RED);
+        }
+
+        knight.tick(GetFrameTime());
+
+        if (knight.getWorldPos().x < 0.f ||
+            knight.getWorldPos().y < 0.f ||
+            knight.getWorldPos().x + windowDimensions[0] > map.width * mapScale ||
+            knight.getWorldPos().y + windowDimensions[1] > map.height * mapScale)
+        {
+            knight.undoMovement();
+        }
+
+        // check collistion
+        for (auto prop : props)
+        {
+            if (CheckCollisionRecs(prop.GetCollisionRec(knight.getWorldPos()), knight.getCollisionRec()))
+            {
+                knight.undoMovement();
+            }
+        }
+
+        for (Enemy *enemy : enemies)
+        {
+            enemy->tick(GetFrameTime());
+        }
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            for (Enemy *enemy : enemies)
+            {
+                if (CheckCollisionRecs(knight.getWeaponCollisionRec(), enemy->getCollisionRec()))
+                {
+                    enemy->setAlive(false);
+                }
+            }
+        }
+
+        EndDrawing();
+    }
+
+    // UnloadTexture(knight_idle);
+    // UnloadTexture(knight_run);
+    // UnloadTexture(knight);
+    UnloadTexture(map);
+    CloseWindow();
+}
